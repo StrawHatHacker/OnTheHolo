fb1c5afcf61b939b66a9db881cfd4d8aba4ee3f8
StrawHatHacker <ntahsp@protonmail.com>
2026-08-14 19:08:23 +0300

    fix

--- app/src/lib/server/db/index.ts
+++ app/src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { env } from '$env/dynamic/private';
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB_NAME } = env;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB_NAME, POSTGRES_HOST } = env;
 
 export const db = drizzle({
 	connection: {
-		connectionString: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DB_NAME}`,
+		connectionString: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB_NAME}`,
 	},
 });
